00229b2c4488394461b423b7863aa3d9f4a91a60
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-21 18:53:45 +0400

    Removed legacy code

--- source/game/CoinBlock.js
+++ source/game/CoinBlock.js
@@ -11,8 +11,6 @@ function CoinBlock(){
 
 CoinBlock.prototype.draw = function(){
     screen.drawImage(this.sprite, 0, 0, 2000, 2100, this.x, this.y, this.width, this.height);
-    /*screen.fillStyle = "#D82126";
-    screen.fillRect(this.x, this.y, this.width, this.height);*/
 }
 
 CoinBlock.prototype.process = function(){
@@ -29,9 +27,9 @@ CoinBlock.prototype.takecoin = function(){
 CoinBlock.prototype.spawnCoin = function(){
   this.x = Math.floor(Math.random() * (canvas.width - this.width));
   this.y = Math.floor(Math.random() * (canvas.height - this.height));
-  for (var i = 0; i < scene.objectsGroup.length; i++){
-    if(scene.objectsGroup[i] == wall){
-      wall = scene.objectsGroup[i]
+  for (var i = 0; i < SCENE.objectsGroup.length; i++){
+    if(SCENE.objectsGroup[i] == wall){
+      wall = SCENE.objectsGroup[i]
       if ((this.x < wall.x+30) & (this.x > wall.x-this.width) & (this.y < wall.y+30) & (this.y > wall.y-this.height)) {console.log("test spawncoin"); coin.spawncoin();}
     }
   }
