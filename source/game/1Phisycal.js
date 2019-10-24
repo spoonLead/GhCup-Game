7bc26f31ea2ba46c9fb31b4f273e243ccfe98e83
SpoonLead <xuxlaevoleg@yandex.ru>
2019-10-24 19:22:50 +0400

    Refactored Top and Down collisions

--- source/game/1Phisycal.js
+++ source/game/1Phisycal.js
@@ -6,23 +6,38 @@ class Phisycal{
     this.height;
   }
 
+
   hasTopCollisionWithClassOfObj(objClass){
     for(var i = 0; i < SCENE.objectsGroup.length; i++){
-      if(SCENE.objectsGroup[i] instanceof objClass){
-        if((SCENE.objectsGroup[i].y+30==this.y) & (SCENE.objectsGroup[i].x > this.x - 30) & (SCENE.objectsGroup[i].x < this.x + this.width))
+      if(SCENE.objectsGroup[i] instanceof objClass)
+        if(this.hasTopCollisionWithObj(SCENE.objectsGroup[i]))
           return true
-      }
     }
   }
 
+  hasTopCollisionWithObj(object){
+    if((object.y+30==this.y) & (object.x > this.x - 30) & (object.x < this.x + this.width))
+      return true
+  }
+
+
+
+
   hasDownCollisionWithClassOfObj(objClass){
     for(var i = 0; i < SCENE.objectsGroup.length; i++){
-      if(SCENE.objectsGroup[i] instanceof objClass){
-        if((SCENE.objectsGroup[i].y==PLAYER.y+PLAYER.height) & (SCENE.objectsGroup[i].x>PLAYER.x-30) & (SCENE.objectsGroup[i].x<PLAYER.x+PLAYER.width))
+      if(SCENE.objectsGroup[i] instanceof objClass)
+        if(this.hasDownCollisionWithObj(SCENE.objectsGroup[i]))
           return true
-      }
     }
   }
+
+  hasDownCollisionWithObj(object){
+    if((object.y==this.y+this.height) & (object.x>this.x-30) & (object.x<this.x+this.width))
+      return true
+  }
+
+
+
 
   hasRightCollisionWithClassOfObj(objClass){
     for(var i = 0; i < SCENE.objectsGroup.length; i++){
