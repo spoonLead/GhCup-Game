f8a8256da67a7a9d1e745a4202709066a610b67e
spoonLead <xuxlaevoleg@yandex.ru>
2019-11-05 18:21:50 +0400

    Added getRandX|YResponseSize funcs

--- source/game/3GameObject.js
+++ source/game/3GameObject.js
@@ -2,4 +2,12 @@ class GameObj extends Drawable{
   constructor(imagePath){
     super(imagePath);
   }
+
+  getRandXResponseSize(){
+    return Math.floor(Math.random() * (canvas.width - this.width));
+  }
+
+  getRandYResponseSize(){
+    return Math.floor(Math.random() * (canvas.height - this.height));
+  }
 }
