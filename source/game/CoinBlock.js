2774757e019fa688c8982597015796792219cdb4
spoonLead <xuxlaevoleg@yandex.ru>
2019-11-12 22:40:42 +0400

    Added hasCoinCollWithWalls func

--- source/game/CoinBlock.js
+++ source/game/CoinBlock.js
@@ -7,19 +7,40 @@ class CoinBlock extends GameObj{
     this.height = 25
   }
 
+  // spawnCoin(){
+  //   this.x = this.getRandXResponseSize()
+  //   this.y = this.getRandYResponseSize()
+  //   for (var i = 0; i < SCENE.objectsGroup.length; i++){
+  //
+  //     if(SCENE.objectsGroup[i] == wall){
+  //       wall = SCENE.objectsGroup[i]
+  //       if ((this.x < wall.x+30) & (this.x > wall.x-this.width) & (this.y < wall.y+30) & (this.y > wall.y-this.height))
+  //         this.spawnCoin()
+  //     }
+  //
+  //   }
+  // }
+
+
   spawnCoin(){
     this.x = this.getRandXResponseSize()
     this.y = this.getRandYResponseSize()
-    for (var i = 0; i < SCENE.objectsGroup.length; i++){
 
+    if(this.hasCoinCoollWithWalls())
+      this.spawnCoin()
+    }
+
+  hasCoinCoollWithWalls(){
+    for (var i = 0; i < SCENE.objectsGroup.length; i++){
       if(SCENE.objectsGroup[i] == wall){
         wall = SCENE.objectsGroup[i]
         if ((this.x < wall.x+30) & (this.x > wall.x-this.width) & (this.y < wall.y+30) & (this.y > wall.y-this.height))
-          this.spawnCoin()
+          return true
       }
-
     }
   }
+
+
 
 
 }
