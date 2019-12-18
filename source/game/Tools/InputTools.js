2e4b10656ea606eaf29faaeb66d548084cb8d53f
spoonLead <xuxlaevoleg@yandex.ru>
2019-12-18 10:28:55 +0400

    Feat(Input): Addded mouse tools

--- source/game/Tools/InputTools.js
+++ source/game/Tools/InputTools.js
@@ -5,3 +5,21 @@ function getLastDownedKey(){
   }
   return this.lastDownedKey
 }
+
+function getMouseX(){
+     this.mouseX;
+     this.onmousemove = function(e){
+          this.mouseX = e.clientX;
+     }
+     return this.mouseX;
+}
+
+
+
+function getMouseY(){
+     this.mouseY;
+     this.onmousemove = function(e){
+          this.mouseY = e.clientY;
+     }
+     return this.mouseY;
+}
