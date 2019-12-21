63d37e36428aec8b3580d91d2cf589718ec8cbc1
spoonLead <xuxlaevoleg@yandex.ru>
2019-12-21 19:25:38 +0400

    Feat(Interface Button): Added button processing

--- source/game/Interface/Button.js
+++ source/game/Interface/Button.js
@@ -1,14 +1,40 @@
-class Button extends Drawable{
-     constructor(x, y, width, height, text){
-          super("")
+class ButtonRestart extends Drawable{
+     constructor(x, y, width, height){
+          super("img/button_restart.png")
           this.x = x
           this.y = y
           this.width = width
           this.height = height
+
+
+          this.pressedWidth = width + 10
+          this.pressedHeight = height + 10
+          this.uppedWidth = width
+          this.uppedHeight = height
      }
 
-     getState(){
-          if(this.hasCollisionWithCoord(this.getMouseX(), this.getMouseY()))
+     processing(){
+          if(this.isPressed()){
+               console.log("ispressed")
+               this.width = this.pressedWidth
+               this.height = this.pressedHeight
+          }
+          if(this.isUpped()){
+               console.log("is upped")
+               this.width = this.uppedWidth
+               this.height = this.uppedHeight
+          }
+     }
+
+     isPressed(){
+          if(this.hasCollisionWithCoord(getMouseX(), getMouseY()) & isMouseDown() == true)
+               return true
+          else
+               return false
+     }
+
+     isUpped(){
+          if(this.hasCollisionWithCoord(getMouseX(), getMouseY()) & isMouseUp() == true)
                return true
           else
                return false
