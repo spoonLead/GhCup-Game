bccd656a44ddea43ea6ce8edc38fde219054de80
spoonLead <xuxlaevoleg@yandex.ru>
2019-12-22 20:43:20 +0400

    Refactoring(Drawable): Added default parameters

--- source/game/Objects/Drawable.js
+++ source/game/Objects/Drawable.js
@@ -6,7 +6,7 @@ class Drawable extends Phisycal{
     this.sprite.src = imagePath;
   }
 
-  draw(x, y, width, height){
+  draw(x = this.x, y = this.y, width = this.width, height = this.height){
     screen.drawImage(this.sprite, 0, 0, this.sprite.width, this.sprite.height, x, y, width, height);
   }
 }
